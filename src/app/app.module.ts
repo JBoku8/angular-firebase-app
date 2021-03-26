04582f0d87378229971e16c58c4fe463a2c37dcb
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-03-27 00:38:53 +0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA0zIui1eVFZ_LR-K0Pc5jTfdRZvdBPzoA',
+    authDomain: 'whatsapp-clone-aa56a.firebaseapp.com',
+    projectId: 'whatsapp-clone-aa56a',
+    storageBucket: 'whatsapp-clone-aa56a.appspot.com',
+    messagingSenderId: '944132038185',
+    appId: '1:944132038185:web:13db1ead82a523beb9c638',
+  },
 };
 
 /*
